51887df063db526e45f7fb6f814cb1ece37028ff
Patar.Tambunan <Patar.Tambunan@itpega01.SINARMASMSIGLIFE.CO.ID>
2019-03-28 16:48:11 +0700

    modify LinksController and links-management

--- src/main/java/com/app/controller/LinksController.java
+++ src/main/java/com/app/controller/LinksController.java
@@ -1,15 +1,32 @@
 package com.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.app.manager.model.Link;
+import com.app.manager.repo.LinkRepository;
+
 @Controller
 @RequestMapping("/links")
 public class LinksController {
+	
+
+	@Autowired
+	private LinkRepository roleRepository;
 
 	@RequestMapping("/links")
 	public String links(Model model){
+		List<Link> links = new ArrayList<Link>();
+		links = (List<Link>)roleRepository.findAll();
+		for(Link l:links){
+			System.out.println(l.getUrl());
+		}
+		model.addAttribute("links",links);
 		return "links-management";
 	}
 
