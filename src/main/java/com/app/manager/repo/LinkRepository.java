e07eb84fb8a43350229ec4cc9748a8ff71930572
Patar.Tambunan <Patar.Tambunan@itpega01.SINARMASMSIGLIFE.CO.ID>
2019-03-28 16:50:15 +0700

    add link and link repository

---
+++ src/main/java/com/app/manager/repo/LinkRepository.java
@@ -0,0 +1,16 @@
+package com.app.manager.repo;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.app.manager.model.Link;
+
+public interface LinkRepository  extends CrudRepository<Link,Long>{
+	public List<Link> findByActive(boolean active);
+	public List<Link> findByActiveAndIsNew(boolean active, boolean n);
+	public List<Link> findByIsNew(boolean n);
+	public List<Link> findByIsNewUrl(boolean n);
+	public Link findByPath(String path);
+}
+
